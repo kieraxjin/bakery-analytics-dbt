88aacd5b168efb59a8a60ed4d4dcb42fecd6a0ad
Kiera Jin <kiera.x.jin@gmail.com>
2026-03-20 15:52:17 -0700

    adding ingredients table

--- Custom Query-19-03-2026-14-44-17.sql
+++ Custom Query-19-03-2026-14-44-17.sql
@@ -1,3 +1,8 @@
+-- ONE: SEE DATA IN ALL TABLS 
+select * from stg_recipes;
+select * from stg_recipe_trials;
+select * from stg_ingredients;
+
+-- TWO: RATING DIFFERENCE BETWEEN VERSIONS 
 select * from fct_recipe_evolution;
 
--- select * from stg_recipes;
